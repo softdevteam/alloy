88f40875878579309c597cb1990332910d09749b
Jake Hughes <jh@jakehughes.uk>
2022-01-10 13:15:45 +0000

    Temporarily bypass `needs_finalizer`
    Also remove `NoFinalize` from tuple implemenations as this can result in
    cyclic trait lookups in trait resolution and cause an ICE.
    
    See issue #52843

--- compiler/rustc_middle/src/ty/util.rs
+++ compiler/rustc_middle/src/ty/util.rs
@@ -882,7 +882,7 @@ impl<'tcx> ty::TyS<'tcx> {
                 // This doesn't depend on regions, so try to minimize distinct
                 // query keys used.
                 let erased = tcx.normalize_erasing_regions(param_env, query_ty);
-                tcx.needs_finalizer_raw(param_env.and(erased))
+                tcx.needs_drop_raw(param_env.and(erased))
             }
         }
     }
