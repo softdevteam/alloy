jacob-hughes/rustgc

Add `FinalizerSafe` auto trait
A `Gc<T>` finalizes values on a separate thread. This means that if `T`
has a drop method, `T` must implement `Send` + `Sync` in order for drop
to be thread-safe.

This can be overly restrictive though, and users may unsafely impl
`Send` / `Sync` on a non-thread-safe `T`  to keep the compiler happy. At
first, it may seem fine to do this if they've verified the drop method
can never use `T` in an unsound way. However, making `T: Send + Sync`,
is a "global" hack, and it can now be erroneously used across threads
outside of a `Gc` - something the user did not intend.

This is where `FinalizerSafe` comes in, it can be thought of as making
`T: Send + Sync` but *only* when it's used in a finalizer. This is still
unsafe - and will need to be carefully checked each time - but it comes
with fewer unintended consequences.

library/core/src/cell.rs
@@ -195,7 +195,7 @@
 use crate::cmp::Ordering;
 use crate::fmt::{self, Debug, Display};
 use crate::gc::NoFinalize;
-use crate::marker::{PhantomData, Unsize};
+use crate::marker::{FinalizerSafe, PhantomData, Unsize};
 use crate::mem;
 use crate::ops::{CoerceUnsized, Deref, DerefMut};
 use crate::ptr::{self, NonNull};
@@ -251,6 +251,9 @@ unsafe impl<T: ?Sized> Send for Cell<T> where T: Send {}
 
 #[unstable(feature = "gc", issue = "none")]
 unsafe impl<T: ?Sized> NoFinalize for Cell<T> where T: NoFinalize {}
+
+#[unstable(feature = "gc", issue = "none")]
+impl<T: ?Sized> !FinalizerSafe for Cell<T> {}
 
 // Note that this negative impl isn't strictly necessary for correctness,
 // as `Cell` wraps `UnsafeCell`, which is itself `!Sync`.
@@ -1157,6 +1160,9 @@ unsafe impl<T: ?Sized> Send for RefCell<T> where T: Send {}
 
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<T: ?Sized> !Sync for RefCell<T> {}
+
+#[unstable(feature = "gc", issue = "none")]
+impl<T: ?Sized> !FinalizerSafe for RefCell<T> {}
 
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<T: Clone> Clone for RefCell<T> {

softdevteam/alloy

library/core/src/cell.rs
@@ -238,7 +238,7 @@
 use crate::cmp::Ordering;
 use crate::fmt::{self, Debug, Display};
 use crate::intrinsics::is_nonoverlapping;
-use crate::marker::{PhantomData, Unsize};
+use crate::marker::{FinalizerSafe, PhantomData, Unsize};
 use crate::mem;
 use crate::ops::{CoerceUnsized, Deref, DerefMut, DispatchFromDyn};
 use crate::ptr::{self, NonNull};
@@ -1234,6 +1234,9 @@ unsafe impl<T: ?Sized> Send for RefCell<T> where T: Send {}
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<T: ?Sized> !Sync for RefCell<T> {}
 
+#[unstable(feature = "gc", issue = "none")]
+impl<T: ?Sized> !FinalizerSafe for RefCell<T> {}
+
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<T: Clone> Clone for RefCell<T> {
     /// # Panics
@@ -2005,6 +2008,8 @@ pub struct UnsafeCell<T: ?Sized> {
 
 #[stable(feature = "rust1", since = "1.0.0")]
 impl<T: ?Sized> !Sync for UnsafeCell<T> {}
+#[unstable(feature = "gc", issue = "none")]
+impl<T: ?Sized> !FinalizerSafe for UnsafeCell<T> {}
 
 impl<T> UnsafeCell<T> {
     /// Constructs a new instance of `UnsafeCell` which will wrap the specified
@@ -2203,6 +2208,9 @@ pub struct SyncUnsafeCell<T: ?Sized> {
 
 #[unstable(feature = "sync_unsafe_cell", issue = "95439")]
 unsafe impl<T: ?Sized + Sync> Sync for SyncUnsafeCell<T> {}
+
+#[unstable(feature = "gc", issue = "none")]
+unsafe impl<T: ?Sized + FinalizerSafe> FinalizerSafe for SyncUnsafeCell<T> {}
 
 #[unstable(feature = "sync_unsafe_cell", issue = "95439")]
 impl<T> SyncUnsafeCell<T> {